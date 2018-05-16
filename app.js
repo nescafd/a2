67054133a34c12a897e02368bc52c338e09c59ba
nescafd <36847667+nescafd@users.noreply.github.com>
2018-05-16 14:29:15 +0100

    second

---
+++ app.js
@@ -0,0 +1,69 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var listtoBuy = this;
+
+  listtoBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+
+  listtoBuy.buy = function (index) {
+    ShoppingListCheckOffService.bought(index);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var listBought = this;
+
+  listBought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var itemsToBuy = [{
+    name: 'cookies',
+    quantity: 2
+  }, {
+    name: 'cream',
+    quantity: 4
+  },{
+    name: 'milk',
+    quantity: 20
+  },{
+    name: 'bread',
+    quantity: 100
+  },];
+
+  var itemsBought = [];
+
+
+  service.bought = function (index) {
+    itemsBought.push(itemsToBuy[index]);
+    itemsToBuy.splice(index,1);
+  };
+
+  service.getItemsToBuy = function ()
+  {
+    return itemsToBuy;
+  };
+
+  service.getBoughtItems = function ()
+  {
+    return itemsBought;
+  };
+}
+
+
+})();
